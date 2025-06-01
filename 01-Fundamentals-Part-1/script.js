f02ed546ab88899b07483fdbde753f2f6abfa483
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-01 01:48:00 -0700

    Operators Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -79,6 +79,7 @@ var job = "Programmer";
 job = "Teachers";
 */
 
+/*
 // Basics Operators
 const now = 2037;
 const ageJonas = now -1991;
@@ -106,3 +107,20 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2019);
+*/
+
+
+// Operators Precedence 
+const now = 2037;
+const ageJonas = now -1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(now - 1991 > now - 2019);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah)/2;
+console.log(ageJonas, ageSarah, averageAge);
