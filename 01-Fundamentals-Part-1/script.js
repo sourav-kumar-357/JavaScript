1943fbf3734546714bba787b4a0baacd75eb68ef
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-01 09:12:24 -0700

    Switch statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -360,7 +360,7 @@ else {
 */
 
 
-
+/*
 // Switch Statements 
 const day = prompt("Please enter the day");
 
@@ -388,4 +388,24 @@ switch (day) {
     default:
         console.log("Not a valid day");
         break;
+}
+*/
+
+
+
+// Some more uses of Switch statements
+const day = prompt("Please enter the day");
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
 }
