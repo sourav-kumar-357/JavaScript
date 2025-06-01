a116806ba42b0721e542826813829971c8b3d5b7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-05-31 22:47:31 -0700

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,24 @@
 let js = 'amazing';
-      if (js === 'amazing') alert ('JavaScript is fun');
+console.log(40 + 67 - 87);
 
-      40 + 67 - 87;
-      console.log(40 + 67 - 87);
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Jonas_Matilda = "JM";
+let _new = 77;
+let name = "Jonas";
+
+let myFisrtJob = "programmer";
+let myCurrentJob = "Teacher";
+
+let Job1 = 'programmer';
+let Job2 = 'Teacher';
+
+console.log(myCurrentJob);
