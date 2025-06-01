2a6441cf4a6d9633f4a9b1db4ac1b6d82c4a0d27
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-01 01:05:54 -0700

    Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -67,6 +67,7 @@ console.log(typeof isIsland);
 console.log(typeof language);
 */
 
+/*
 // Types of Declaring a variables
 let age = 30;
 age = 31;
@@ -76,6 +77,32 @@ birthYear = 2000;
 
 var job = "Programmer";
 job = "Teachers";
+*/
 
+// Basics Operators
+const now = 2037;
+const ageJonas = now -1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
 
-//
+console.log(ageJonas*2, ageJonas/10, 2**3);
+// 2**3 means that the 2 to the power of 3 which is equal to 2 * 2 * 2
+
+const firstName = "Sourav";
+const lastName = "Kumar";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1 = 101
+x--;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah); // >,<,>=,<=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2019);
