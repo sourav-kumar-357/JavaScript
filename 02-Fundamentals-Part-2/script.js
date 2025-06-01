d8f6e4e0559f39b4602d4df3518ec60ff9239cf5
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-01 12:40:35 -0700

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -15,3 +15,25 @@ if (hasDriversLicense) {
 // const private = 534;
 */
 
+
+
+// Functions
+function logger() {
+    console.log('My name is Jonas');
+}
+// calling invoking or running a function
+logger();
+logger();
+logger();
+
+// Second Functions
+function fruitProcessors(apples, oranges ) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessors(5, 0));
+
+const appleOrangeJuice = fruitProcessors(2, 4);
+console.log(appleOrangeJuice);
