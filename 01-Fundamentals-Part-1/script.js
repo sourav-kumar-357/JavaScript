fac2738cf09449d9abb2728088596d7129a19499
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-01 07:10:56 -0700

    Challenge 3

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -344,3 +344,16 @@ console.log(hasDriversLicense3 || hasGoodVision3);
 
 
 // Coding Challenges 3
+const scoreDolphins = (97+112+80)/3;
+const scoreKoalas = (100+95+50)/3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy');
+
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Both win the trophy');
+} else if (scoreDolphins == scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100);
+else {
+    console.log('No one win the trophy');
+}
