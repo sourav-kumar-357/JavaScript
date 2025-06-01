f6aec6b5eb9ecffcdd8001329011d5fc6fe63b5f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-05-31 23:18:35 -0700

    datatypes

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -35,3 +35,26 @@ console.log("Country is " + Country);
 console.log("The population is " + population);
 */
 
+/*
+// Datatypes
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 'Jonas');
+console.log(typeof 23);
+
+javascriptIsFun = "Yes!"
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
