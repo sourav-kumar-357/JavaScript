4c5edb7eac1d985dbf7e8d31635c25269099bbae
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 05:22:52 -0700

    Functions calls Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -119,5 +119,32 @@ console.log(yearsUntilRetirements(1920, 'Sourav'));
 */
 
 
+/*
+// Coding Challenge 01
+const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-// 
+// Test 1
+let scoreDolphins = calcAverage(0, 0, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
