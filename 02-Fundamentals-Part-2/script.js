eb83796f4d153146f49b0e564a3030b4bbd51c47
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 09:13:14 -0700

    Coding Challenge

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -272,7 +272,7 @@ console.log(Sourav);
 
 
 
-
+/*
 // Dot vs bracket Notations
 
 // This is a Function
@@ -310,3 +310,41 @@ if (Sourav[intrestedIn]) {
 Sourav.location = 'India';
 Sourav['State'] = 'Jharkhand';
 console.log(Sourav);
+*/
+
+
+/*
+// Challenges 
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+}
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  }
+}
+
+john.calcBMI();
+mark.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+*/
