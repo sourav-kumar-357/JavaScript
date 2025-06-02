f9271c3e7b963cf7d68e68f5c741fb48e5cd510d
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 09:36:43 -0700

    While Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -410,6 +410,7 @@ console.log(ages);
 */
 
 
+/*
 //LOOPING BACKWARDS
   const Anshu = [
     'Anshu',
@@ -432,4 +433,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
   }
+}
+*/
+
+
+
+// WHILE LOOP
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) { 
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
 }
