84f49a0dbd06b32bef9a1adbd13da3a087eb71a1
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 06:10:47 -0700

    Methods and Arrays

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -230,7 +230,7 @@ if (friends.includes('Priya')) {
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-  
+
 // const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bills = [125, 555, 44];
@@ -238,4 +238,35 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+*/
+
+
+
+/*
+// OBJECTS
+//DOT vs BRACKET NOTATION
+
+
+// OBJECTS
+//DOT vs BRACKET NOTATION
+
+//This is a normal array
+const Anshu = [
+    'Anshu',
+    'Sharma',
+    2025-2006,
+    'Student',
+    ['Vasu', 'Farhan', 'Asif']
+]
+console.log(Anshu);
+
+// This is a Function
+const Sourav = {
+    firstName: 'Sourav',
+    lastName: 'Kumar',
+    age: 2025-2006,
+    profession: 'Student',
+    friends: ['Vasu', 'Farhan', 'Asif']
+};
+console.log(Sourav);
 */
