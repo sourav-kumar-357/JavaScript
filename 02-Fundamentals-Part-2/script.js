99d7b807716f9cc8f6c7de8fd3a168738ba81cc7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 09:41:25 -0700

    Updated

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -486,3 +486,10 @@ console.log(calcAverage([2, 3, 7]));
 console.log(calcAverage(totals));
 console.log(calcAverage(tips));
 */
+
+
+
+
+/*
+<<<-------------------------[ Completed ]----------------------------->>>
+*/
