166199e7c3c683e1043252bd7a0d31aa5dc05ae8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 05:59:56 -0700

    Challenge 2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -190,7 +190,7 @@ console.log(age);
 */
 
 
-
+/*
 // Basics Arrays operators
 
 // Add new elements 
@@ -222,3 +222,20 @@ console.log(friends.includes('Gadha'));
 if (friends.includes('Priya')) {
     console.log('You have a friend called Priya');
 }
+*/
+
+
+/*
+// Coding Challenge #2
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+  
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+*/
