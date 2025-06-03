341360fccfafa2b4a1f6c149c02b43f3721f4fda
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-03 15:12:42 -0700

    This keyword

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -40,6 +40,8 @@ calcAge(1991);
 // printAge();
 */
 
+
+
 /*
 // Hoisting and TDZ in Practice
 
@@ -86,3 +88,40 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
 */
+
+
+
+
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
