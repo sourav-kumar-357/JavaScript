99de46dc933ed01ed461b32ee3c6ed8f2a0c7bcb
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 23:26:53 -0700

    Number is big

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -23,5 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct Number!';
-  }
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '☝ Too High !';
+  }  
 });
