0b38dab5ae844dd6a15a188fd60202e6b2effeea
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-03 00:10:35 -0700

    Scores

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -26,12 +26,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '☝ Too High !';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☝ Too High !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '👇 Too Low !';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👇 Too Low !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
