cb30fc8a75e0fee17bc94e3bfea82e8f47a93dc3
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-02 23:03:27 -0700

    The Secret number

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -12,11 +12,14 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!'
+    document.querySelector('.message').textContent = '⛔ No Number!';
   }
 });
