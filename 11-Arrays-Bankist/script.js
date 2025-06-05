02b1de5b8e319148b8c10b18c130c5a494539da1
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-05 05:57:10 -0700

    FIND METHOD

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -335,3 +335,49 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
 */
+
+//=================================================================================================================================================//
+
+// --------------------- CODING CHALLENGE #03 --------------------//
+/*
+const calcAverageHumanAge2 = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+
+  return average;
+};
+
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+*/
+
+
+//=================================================================================================================================================//
+
+// --------------------- THE FIND METHOD --------------------//
+/*
+// THE FIND METHOD
+const firstWithdrawl = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawl);
+
+console.log(accounts);
+
+const  account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+*/
+
