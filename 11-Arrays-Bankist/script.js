57fbd0e9ee520b229594170be9605465b041ab75
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-05 05:54:12 -0700

    CHAINING METHOD

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -318,4 +318,20 @@ console.log(avg1, avg2);
 
 //=================================================================================================================================================//
 
-// ---------------------  --------------------//
+// --------------------- CHAINING METHOD --------------------//
+/*
+// THE MAGIC OF CHAINING METHOD
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
