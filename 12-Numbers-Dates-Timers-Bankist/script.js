29b4ba4c2dfaede80dd4cd0333f35c18bc517e20
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 04:40:26 -0700

    Timers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -505,3 +505,25 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(num1)
 );
+
+
+//=================================================================================================================================================//
+
+// --------------------- Timers --------------------//
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
