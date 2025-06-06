eeab771c728f4f6c3fb357551d1737fa2291247b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 04:37:46 -0700

    OPERATION WITH DATES

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -470,3 +470,16 @@ console.log(future);
 
 
 //=================================================================================================================================================//
+
+// --------------------- OPERATION WITH DATES --------------------//
+
+
+const future1 = new Date(2037, 10, 19, 15, 23);
+console.log(+future1);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+
