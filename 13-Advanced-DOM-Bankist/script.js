e45f53a78f6756bf98adaec6ca584b8bbf883ddd
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 02:49:31 -0700

    Lifecycle DOM Events

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -366,3 +366,23 @@ imgTargets.forEach(img => imgObserver.observe(img));
 }
 
 slider();
+
+
+//=================================================================================================================================================//
+
+// --------------------- Lifecycle DOM Events --------------------//
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
+
