584a42be5504f0ddc5a496f62d87cf0c404429a9
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 23:19:44 -0700

    Interacting with Forms

--- 19-DOM-Tutorials-Net-Ninja/Script.js
+++ 19-DOM-Tutorials-Net-Ninja/Script.js
@@ -176,7 +176,30 @@ list.addEventListener('click', (e) => {
 
 // --------------------- Interacting with Forms --------------------//
 
+list = document.querySelector('#book-list ul');
 
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+const forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+  console.log(form);
+});
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
 
 //=================================================================================================================================================//
 
