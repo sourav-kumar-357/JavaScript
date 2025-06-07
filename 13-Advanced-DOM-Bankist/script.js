360e9bc6ed0397f93f4a0b72443580b7017ef0c8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 02:46:07 -0700

    REVEALING ELEMENTS ON SCROLL

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -217,3 +217,33 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 */
+
+//=================================================================================================================================================//
+
+// --------------------- REVEALING ELEMENTS ON SCROLL --------------------//
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+
+//=================================================================================================================================================//
+
+// --------------------- REVEALING ELEMENTS ON SCROLL --------------------//
