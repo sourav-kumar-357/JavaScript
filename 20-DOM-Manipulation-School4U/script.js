14b4f3e0ea609b4fc6b760c70426e20a402cabd7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 05:51:54 -0700

    Events Listners

--- 20-DOM-Manipulation-School4U/script.js
+++ 20-DOM-Manipulation-School4U/script.js
@@ -5,13 +5,12 @@ console.log("hello javascript");
 // --------------------- INTRODUCTION  --------------------//
 
 /*
-There are 4 phases of DOM manipulation
-
 1. DOM (Document Object Model) ✅
 2. Selection of HTML elements ✅
 3. How to change or manipulate HTML ✅
-4. manipulating CSS
-4. Events Listeners (Event ko kaise suna jayega )
+4. manipulating CSS ✅
+5. Events Listeners (Event ko kaise suna jayega ) ✅
+
 */
 
 
@@ -44,3 +43,20 @@ headings1.style.backgroundColor = 'Green';
 // manipulating CSS 
 let para1 = document.querySelector('p');
 para1.style.backgroundColor = 'brown';
+
+
+//=================================================================================================================================================//
+
+// --------------------- Events Listners --------------------//
+
+let h1 = document.querySelector('h1');
+
+// For adding even listners we ahve to write down addEventListners
+// eventlistners take two parameters 
+// addEventListners('event', function() {})
+
+h1.addEventListener('click', () => {
+     h1.style.backgroundColor = 'yellow'; // on click it will change the color
+     h1.style.color = 'black'; // on click it will change the color
+     h1.innerHTML = 'Meri girlFriend bahut he jyada sundar hai ';
+})
