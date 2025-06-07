0adcb0220d5aaed85fc88d040d4dafe42d66db86
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 02:37:10 -0700

    CREATING AND INSERTING ELEMENTS

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -50,3 +50,36 @@ console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
 */
+
+
+//=================================================================================================================================================//
+
+// --------------------- CREATING AND INSERTING ELEMENTS --------------------//
+
+/*
+// CREATING AND INSERTING ELEMENTS
+
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie"> Got it!</button>';
+
+//header.prepend(message);
+header.append(message);
+//header.append(message.cloneNode(true));
+ 
+//header.before(message);
+//header.after(message);
+
+// DELETE ELEMENTS
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  //message.remove();
+  message.parentElement.removeChild(message);
+});
+*/
+
+
+//=================================================================================================================================================//
+
+// --------------------- CREATING AND INSERTING ELEMENTS --------------------//
