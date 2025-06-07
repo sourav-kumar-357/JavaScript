e8435a84083fec7551f5b96c69afdf3484ad1d1e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 02:44:06 -0700

    THE INTERSECTION OBSERVER API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -190,3 +190,30 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
 */
+
+
+//=================================================================================================================================================//
+
+// --------------------- THE INTERSECTION OBSERVER API --------------------//
+
+/*
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+*/
