6e765306feacc3a5fbbe423cc0852ba6d09dddcc
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 05:25:45 -0700

    Intro to DOM

--- 20-DOM-Manipulation-School4U/script.js
+++ 20-DOM-Manipulation-School4U/script.js
@@ -1,13 +1,20 @@
 console.log("hello javascript");
 
-/*
+//=================================================================================================================================================//
 
+// --------------------- INTRODUCTION  --------------------//
+
+/*
 There are 4 phases of DOM manipulation
 
-1. DOM
+1. DOM (Document Object Model) ✅
 2. Selection of HTML elements
 3. How to change or manipulate HTML
 4. manipulating CSS
-4. Events Listeners
-
+4. Events Listeners (Event ko kaise suna jayega )
 */
+
+
+//=================================================================================================================================================//
+
+// --------------------- COMPLETED --------------------//
