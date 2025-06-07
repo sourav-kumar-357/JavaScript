e0f507777310f2d9f678b2b0a5069586287f737b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 23:17:37 -0700

    events

--- 19-DOM-Tutorials-Net-Ninja/Script.js
+++ 19-DOM-Tutorials-Net-Ninja/Script.js
@@ -136,7 +136,27 @@ bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool 
 
 // --------------------- Events --------------------//
 
+const listItems = document.querySelectorAll('#book-list ul li');
 
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
 
 //=================================================================================================================================================//
 
