18b6b4d1939498b1c7ea8dbd2be301dcc15075fb
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 22:28:04 -0700

    Get Element By class name and tags

--- 19-DOM-Tutorials-Net-Ninja/Script.js
+++ 19-DOM-Tutorials-Net-Ninja/Script.js
@@ -12,3 +12,28 @@ console.log(search, bookList);
 //=================================================================================================================================================//
 
 // --------------------- Get Element By Class --------------------//
+
+const titles = document.getElementsByClassName('title');
+console.log(titles);
+console.log(titles[0]);
+console.log(titles[1]);
+
+
+//=================================================================================================================================================//
+
+// --------------------- Get Element By Tag name --------------------//
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+console.log(lis[1]);
+console.log(lis[2]);
+console.log(lis[3]);
+
+// For loop to access all the elements
+console.log('\nFor loop to access all the elements ');
+for (let index = 0; index < lis.length; index++) {
+     const element = lis[index];
+     console.log(element);
+}
+
