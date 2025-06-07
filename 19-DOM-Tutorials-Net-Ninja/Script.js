8fcb8d0caeeaf437f3f9d7dc63fd7eb6e4e82624
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 23:18:36 -0700

    even building

--- 19-DOM-Tutorials-Net-Ninja/Script.js
+++ 19-DOM-Tutorials-Net-Ninja/Script.js
@@ -162,7 +162,15 @@ link.addEventListener('click', function(e){
 
 // --------------------- Events bubling --------------------//
 
+const list = document.querySelector('#book-list ul');
 
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
 
 //=================================================================================================================================================//
 
