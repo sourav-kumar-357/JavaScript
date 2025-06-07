776b328040606b010e423fd45c93f067e039b41f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-06 23:14:44 -0700

    Nodes

--- 19-DOM-Tutorials-Net-Ninja/Script.js
+++ 19-DOM-Tutorials-Net-Ninja/Script.js
@@ -84,7 +84,14 @@ bookList.innerHTML += '<p>This is how you add HTML content</p>';
 
 // --------------------- Nodes --------------------//
 
+const banner = document.querySelector('#page-banner');
 
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
 
 //=================================================================================================================================================//
 
