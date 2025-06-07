8ceacdd8a5ccdca42c2ac0a25f112425d9629fc3
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-07 01:02:00 -0700

    Querry Selector

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+//=================================================================================================================================================//
+
+// --------------------- Lecture Codes --------------------//
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -29,3 +30,23 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//=================================================================================================================================================//
+
+// --------------------- Querry Selectors --------------------//
+
+/*
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+*/
