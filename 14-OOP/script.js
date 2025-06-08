2120771dfb2685dc423d24107cdf81f9b8c1035e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-08 01:43:32 -0700

    Coding Challenge 1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -85,3 +85,30 @@ Array.prototype.unique = function () {
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
+
+
+//===========================================================================================================================================================//
+
+// Coding Challenge 1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('Toyota', 120);
+const mercedes = new Car('Tesla', 95);
+
+Tesla.accelerate();
+Tesla.accelerate();
+Tesla.brake();
+Tesla.accelerate();
