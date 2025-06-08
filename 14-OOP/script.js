f7514c88df398ad7d869faea7216da9869b2407b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-08 07:44:35 -0700

    Coding Challenge 0

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -307,3 +307,47 @@ console.dir(Student.prototype.constructor);
 console.log(Akanksha.__proto__);
 console.log(Akanksha.__proto__.__proto__);
 
+
+//===========================================================================================================================================================//
+
+// Coding Challenge 03
+Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+const Kanchan = new EV('Kanchan', 144, 15);
+Kanchan.chargeBattery(87);
+console.log(Kanchan);
+Kanchan.brake();
+Kanchan.accelerate();
