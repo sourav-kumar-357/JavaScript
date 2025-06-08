ba4b55d3e5d8bdf3925836a15325a9f2d23770a4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-08 01:40:01 -0700

    Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //===========================================================================================================================================================//
 
 // CONSTRUCTOR FUNCTION AND NEW OPERATOR 
@@ -56,3 +57,31 @@ console.log(Sara.species, Mike.species);
 
 console.log(Mike.hasOwnProperty('firstName'));
 console.log(Sara.hasOwnProperty('species'));
+
+
+//===========================================================================================================================================================//
+
+// PROTOTYPAL INHERITANCE 
+console.log(Anshu.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+// Object.prototype (top of prototype chain)
+console.log(Sara.__proto__.__proto__);
+console.log(Sara.__proto__.__proto__.__proto__);
+
+// new Array === []
+const arr = [3, 6, 6, 5, 6, 9, 9]; 
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+console.log(arr.unique());
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
