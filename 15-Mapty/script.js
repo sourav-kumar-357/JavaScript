ee26e5a72267552626dd582cf0ef3b86065c4bb4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-09 20:28:00 +0530

    Set Description Class

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -26,3 +26,21 @@ class Workout {
     this.clicks++;
   }
 }
+
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    // this.type = 'cycling';
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
