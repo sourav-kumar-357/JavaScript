10d09cc2e282fb745fcec00db2b576c28d816482
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-09 21:54:45 +0530

    render Workout

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -234,4 +234,22 @@ class App {
       )
       .openPopup();
   }
+
+  _renderWorkout(workout) {
+    let html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+          }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+    `;
 }
