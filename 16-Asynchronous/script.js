7813c2d4818cb149be30cfcc49ce3ad6f93e0c64
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-10 16:27:12 +0530

    Challenge corrected

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -232,21 +232,25 @@ const whereAmI = function (lat, lng) {
       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
       return res.json();
     })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
     .then(data => {
       console.log(data);
       console.log(`You are in ${data.city}, ${data.countryCode}`);
 
       return fetch(`https://restcountries.com/v2/name/${data.country}`);
     })
-    .then(res => {
-      if (!res.ok) throw new Error(`Country not found (${res.status})`);
-
-      return res.json();
-    })
     .then(data => renderCountry(data[0]))
     .catch(err => console.error(`${err.message} 💥`));
 };
-whereAmI(52.508, 13.381);
+
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+whereAmI(52.508, 13.381);
 */
+
+
+//===========================================================================================================================================================//
