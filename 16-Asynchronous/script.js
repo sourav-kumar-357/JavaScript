463f9a65a959fa4e21deaa503d282690b55ac534
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-10 16:28:08 +0530

    Event Loop

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -252,5 +252,16 @@ whereAmI(-33.933, 18.474);
 whereAmI(52.508, 13.381);
 */
 
-
 //===========================================================================================================================================================//
+
+// The Event Loop 
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
