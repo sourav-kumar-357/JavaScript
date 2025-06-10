a2719bdbc588d899f91f1efa04b9b5c64b05e9a1
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-10 16:12:24 +0530

    assign buttons

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -10,3 +7,6 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
