25d1ebbdd50c61469a57ef9484249b5cacd9bb02
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-21 18:30:34 +0530

    Question 2 solved

--- Revision Course apna college/script1.js
+++ Revision Course apna college/script1.js
@@ -1,6 +1,43 @@
-
+/*
 // Basics of JavaScripts
-console.log('Hello there! My first JS Revision Code');
-console.log('My name is Sourav Kumar');
-console.log('I love JavaScript');
+console.log("Hello there! My first JS Revision Code");
+console.log("My name is Sourav Kumar");
+console.log("I love JavaScript");
+*/
 
+/*
+// Variables of the JavaScript
+const age = 24;
+console.log(`The age is ${age}`);
+const fullName = "Sourav Kumar";
+const job = "Student";
+console.log(`My name is ${fullName} and I am a ${job}`);
+
+// taking inputs from the user
+let isFollow = prompt("Do you dollow Apna College on LinkedIn? (Yes/No)");
+if (isFollow === "yes") {
+     console.log(`I follow apna College`);
+} else {
+     console.log(`I don't follow apna college on LinkedIn`);
+}
+*/
+
+/*
+// Question 1 -->> Create a constant object called product to store the information there regarding it
+const product = {
+     heading: "Parker Jotter Standard CT Ball Pen",
+     rating: 4,
+     current_cost: 270,
+};
+console.log(product);
+*/
+
+/*
+// Question 1 -->> Get user to input a number and check if the number is divisible by 5 or not
+const number = prompt("Please enter the number you wana check");
+if (number % 5 == 0) {
+     console.log("The number is a multiple of 5");
+} else {
+     console.log("the number is not the multiple of 5");
+}
+*/
